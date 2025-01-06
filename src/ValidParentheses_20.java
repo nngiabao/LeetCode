3bbde7b15e15dc2632d799086c6eab85e2256fd4
nngiabao <Giabao@50>
2025-01-05 21:00:11 -0500

    Stack

---
+++ src/ValidParentheses_20.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class ValidParentheses_20 {
+    public static void main(String[] args) {
+        String s="(])";
+        System.out.println(isValid(s));
+    }
+
+    //solution 1
+    public static boolean isValid(String s) {
+        int i=0;
+        Stack<Character> stack = new Stack();
+        while(i<s.length()){
+            char temp = s.charAt(i);
+            if(temp=='(' || temp=='{' || temp=='['){
+                stack.push(temp);
+            }else{
+                if (stack.isEmpty()) return false;
+                char top = stack.peek();
+                if(((top=='(') && (temp==')')) || ((top=='{') && (temp=='}')) ||
+                        ((top=='[') && (temp==']'))){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+
+            }
+            i++;
+        }
+        return stack.isEmpty();
+    }
+}
