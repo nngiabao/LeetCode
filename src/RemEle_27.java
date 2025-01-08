bcbaf03f882ce495d3f873a23bae5a547361d04b
nngiabao <Giabao@50>
2025-01-07 23:48:45 -0500

    Aray

---
+++ src/RemEle_27.java
@@ -0,0 +1,16 @@
+public class RemEle_27 {
+    public static void main(String[] args) {
+
+    }
+
+    public int removeElement(int[] nums, int val) {
+        int count=0;
+        int []temp=new int[nums.length];
+        for(int i: nums) temp[count++]=i;
+        count =0;
+        for(int i: temp){
+            if(i!= val) nums[count++]=i;
+        }
+        return count;
+    }
+}
---
+++ src/TriMaxNumber_414.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static java.lang.Math.abs;
+
+public class TriMaxNumber_414 {
+    public static void main(String[] args) {
+        int[]nums = {-2147483648,-2147483648,-2147483648,-2147483648,1,1,1};
+        System.out.println(thirdMax(nums));;
+    }
+
+    //Solution 1
+    public static int thirdMax(int[] nums) {
+        //if dup => skip
+        Integer max1=null,max2=null,max3=null;
+        for(Integer i:nums){
+        if(i == (max3) || i == (max2 )  || i == (max1 ) ) continue;
+        if (max1==null || i > max1 ) {
+                max3 = max2;
+                max2 = max1;
+                max1 = i;
+            }else if( max2== null || i>max2){
+                max3 = max2;
+                max2 = i;
+            }else if(max3== null || i>max2){
+                max3 = i;
+            }
+        }
+        return max3==null ? max1 : max3;
+    }
+}
