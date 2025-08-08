c1c09e171e4f2a813c065371856b3d668d3bbc26
Bao <nngiabao4@gmail.com>
2025-08-08 14:18:52 -0400

    Random

---
+++ src/Queue/StudentsSanwichs_1700.java
@@ -0,0 +1,35 @@
+package Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class StudentsSanwichs_1700 {
+    public int countStudents(int[] students, int[] sandwiches) {
+        //
+        int i = 0;
+        Queue<Integer> q = new LinkedList<Integer>();
+        //put in a queue
+        for (int num : students) {
+            q.add(num);
+        }
+        //
+        while (i < sandwiches.length) {
+            boolean flag = true;
+            int size = q.size();
+            for (int j = 0; j < size; j++) {
+                int student = q.poll();
+                if (students[i] == sandwiches[i]) {
+                    q.add(i);
+                    i++;
+                    flag = false;
+                    break;
+                } else {
+                    q.add(student);
+                }
+            }
+            if(flag) return q.size();
+        }
+        //
+        return 0;
+    }
+}
