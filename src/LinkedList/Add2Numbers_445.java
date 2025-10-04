d18cc32648422a7be426ee192908131d12119030
Bao <nngiabao4@gmail.com>
2025-10-04 12:58:25 -0400

    Random

--- src/LinkedList/Add2Numbers_445.java
+++ src/LinkedList/Add2Numbers_445.java
@@ -38,15 +38,57 @@ public class Add2Numbers_445 {
             pointer_2 = pointer_2.next;
         }
         ListNode newList = new ListNode(0);
+        ListNode pointer_new = newList;
         List<Integer> list = new ArrayList<>();
-        while(!stack_1.isEmpty() || !stack_2.isEmpty()){
+        int carry =0;
+        while(!stack_1.isEmpty() || !stack_2.isEmpty() || carry != 0){
             int val_1 = stack_1.isEmpty() ? 0 : stack_1.pop();
             int val_2 = stack_2.isEmpty() ? 0 : stack_2.pop();
-            int sum = val_1 + val_2;
-            int carry = sum/10;
-
+            int sum = val_1 + val_2+carry;
+            carry = sum/10;
+            list.add(sum%10);
         }
         //build the linked list
+        for (int i=list.size()-1;i>=0;i--) {
+            pointer_new.next = new ListNode(list.get(i));
+            pointer_new = pointer_new.next;
+        }
         return newList.next;
+    }
+    //Solution 2:
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode l1_rev = rev(l1);
+        ListNode l2_rev = rev(l2);
+        int carry = 0;
+        ListNode newList = null;
+        while(l1_rev != null || l2_rev != null || carry > 0){
+            //
+            if(l1_rev != null){
+                carry += l1_rev.val;
+                l1_rev = l1_rev.next;
+            }
+            //
+            if(l2_rev != null){
+                carry += l2_rev.val;
+                l2_rev = l2_rev.next;
+            }
+            ListNode n = new ListNode(carry % 10);
+            carry /= 10;
+            n.next = newList;
+            newList = n;
+        }
+
+        return newList;
+    }
+
+    private ListNode rev(ListNode l){
+        ListNode prev = null;
+        while(l != null){
+            ListNode t = l.next;
+            l.next = prev;
+            prev = l;
+            l = t;
+        }
+        return prev;
     }
 }
