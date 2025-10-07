becdb644ea2b1afe045bd6425a712dd1a50c22db
Bao <nngiabao4@gmail.com>
2025-10-07 11:43:40 -0400

    BST

---
+++ src/BST/MinLimitBalls_1760.java
@@ -0,0 +1,10 @@
+package BST;
+
+import java.util.*;
+
+public class MinLimitBalls_1760 {
+    public int minimumSize(int[] nums, int maxOperations) {
+
+        return 12;
+    }
+}
---
+++ src/BST/MyCalendar1_704.java
@@ -0,0 +1,23 @@
+package BST;
+
+import java.util.*;
+
+public class MyCalendar1_704 {
+    //using arraylsit
+    List<int[]> list;
+    public MyCalendar1_704() {
+        list = new ArrayList<>();
+    }
+
+    public boolean book(int startTime, int endTime) {
+        if(list.size() == 0) list.add(new int[]{startTime, endTime});
+        else{
+            for(int[] book : list){
+                int start = book[0], end = book[1];
+                if(Math.max(start,startTime) < Math.min(end,endTime)) return false;
+                list.add(new int[]{startTime, endTime});
+            }
+        }
+        return true;
+    }
+}
