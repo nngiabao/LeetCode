de8810e36216e8e74b77bf2964ea503c81cface7
Bao <nngiabao4@gmail.com>
2025-10-09 11:47:03 -0400

    BST

--- src/String/FindReplacePattern_890.java
+++ src/String/FindReplacePattern_890.java
@@ -1,22 +1,38 @@
 package String;
 
 import java.util.*;
+
 public class FindReplacePattern_890 {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
         List<String> result = new ArrayList<>();
-        //build the pattern
-        Set<Character> set = new HashSet<>();
-        for(char c : pattern.toCharArray()) {
-            set.add(c);
-        }
-        //
-        for(String w : words) {
-            Set<Character> temp = new HashSet<>();
-            for(char c : w.toCharArray()) {
-                temp.add(c);
+
+        for (String word : words) {
+            if (matches(word, pattern)) {
+                result.add(word);
             }
-            if(set.size() == temp.size()) result.add(w);
         }
         return result;
+    }
+
+    private boolean matches(String word, String pattern) {
+        if (word.length() != pattern.length()) return false;
+
+        Map<Character, Character> map1 = new HashMap<>();
+        Map<Character, Character> map2 = new HashMap<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            char w = word.charAt(i);
+            char p = pattern.charAt(i);
+
+            // If already mapped, must be consistent
+            if (map1.containsKey(p) && map1.get(p) != w) return false;
+            if (map2.containsKey(w) && map2.get(w) != p) return false;
+
+            // Otherwise create mapping
+            map1.put(p, w);
+            map2.put(w, p);
+        }
+
+        return true;
     }
 }
