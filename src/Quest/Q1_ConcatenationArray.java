9060b6337e5874efa03580e79f773eb6a7b5ce45
Bao <nngiabao4@gmail.com>
2025-11-25 12:16:01 -0500

    Random

---
+++ src/Quest/Q1_ConcatenationArray.java
@@ -0,0 +1,15 @@
+package Quest;
+
+public class Q1_ConcatenationArray {
+    public int[] getConcatenation(int[] nums) {
+        //
+        int len = nums.length;
+        int[] result = new int[len*2];
+        //
+        for(int i = 0; i < result.length; i++){
+            result[i] = nums[i%len];
+        }
+        //
+        return result;
+    }
+}
